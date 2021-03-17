44b590cd23c49c9d676a160d55e66dd2eeffabf1
JacobLoredo <yeicob_loredo@hotmail.com>
2021-03-17 10:52:10 -0600

    cambio de letra somos

--- resources/views/Nosotros/Tabla.blade.php
+++ resources/views/Nosotros/Tabla.blade.php
@@ -1,7 +1,7 @@
 <div class="tab-content" id="tab-content-nosotros">
     <div class="tab-pane fade show active" id="Historia" role="tabpanel" aria-labelledby="v-pills-home-tab">
         @include('Nosotros.Historia')
-        @include('Nosotros.Quienes-Somos')
+        @include('Nosotros.Quienes-somos')
         @include('Nosotros.Mision-Vision')
         @include('Nosotros.Normativa')
         @include('Nosotros.Directorio')
