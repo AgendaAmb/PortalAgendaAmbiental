f47e84d8cd994bb6f705312cacbf9604a6ec8c1c
MickeyMenor <miguel.menor09@gmail.com>
2021-06-15 10:00:01 -0500

    "COmmit de verificación"

--- resources/views/Parciales/header.blade.php
+++ resources/views/Parciales/header.blade.php
@@ -1,23 +1,23 @@
 <nav id="header" class="p-2">
     <div>
-        <a href={{route('Index')}}><img src={{ asset('storage/imagenes/Logos/INICIO-AGENDA-300x127.png')}} class="imagenAgenda px-5 pt-4 px-xl-5 px-lg-5 px-md-1"></a>        
+        <a href={{route('Index')}}><img src={{ asset('storage/imagenes/Logos/INICIO-AGENDA-300x127.png')}} class="imagenAgenda px-5 pt-4 px-xl-5 px-lg-5 px-md-1"></a>
     </div>
     <div class="">
-        <a href={{route('Index')}}><img src={{ asset('storage/imagenes/Logos/horizontal_blanco.webp')}} class="img-fluid imagenAgenda_uaslp  pt-3"></a>        
+        <a href={{route('Index')}}><img src={{ asset('storage/imagenes/Logos/horizontal_blanco.webp')}} class="img-fluid imagenAgenda_uaslp  pt-3"></a>
     </div>
     <div>
         <a href="http://www.uaslp.mx"><img src={{ asset('storage/imagenes/Logos/LOGO_UASLP-300x116.png')}} class="imagenUASLP px-5 pt-4 px-xl-5 px-lg-5 px-md-1"></a>
     </div>
 </nav>
 
-{{-- 
+{{--
     Páginas pendientes de Comunicación:
- 
+
 
 
     2.- http://ambiental.uaslp.mx/conferencias/index.html
-        Ciclo de conferencias a distancia, 
+        Ciclo de conferencias a distancia,
         Abril del 2020.
 
-    3.- 
+    3.- Por definir
 --}}
