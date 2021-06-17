c9462ac858d9aa3a2dc218f44f2e5cd81b965392
JacobLoredo <yeicob_loredo@hotmail.com>
2021-06-17 18:39:48 -0500

    cambios se scrip para que todo funcione

--- resources/views/Parciales/head.blade.php
+++ resources/views/Parciales/head.blade.php
@@ -8,9 +8,11 @@
 <!--Icono de la aplicación-->
 <link rel="shortcut icon" href="{{ asset('img/cropped-UASLPAgendaAmbiental-32x32.png') }}">
 <!-- Scripts -->
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
 </script>
+<script src="{{ asset('js/app.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
     integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
 </script>
