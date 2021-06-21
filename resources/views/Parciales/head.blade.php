08168aeff2d81f189eafa3683f44c20941a37f54
JacobLoredo <yeicob_loredo@hotmail.com>
2021-06-21 11:27:47 -0500

    cambio en banner y boton leer mas

--- resources/views/Parciales/head.blade.php
+++ resources/views/Parciales/head.blade.php
@@ -12,6 +12,7 @@
 <script async src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
 </script>
+<!--estilos-->
 <script async src="{{ asset('js/app.js') }}"></script>
 <script async src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
     integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
