464ef052d37316c24d81452b2326e280e6e5620b
JacobLoredo <yeicob_loredo@hotmail.com>
2021-06-29 11:01:16 -0500

    acomodo de wheel

--- resources/views/components/eje-de-trabajo.blade.php
+++ resources/views/components/eje-de-trabajo.blade.php
@@ -1,4 +1,4 @@
-<div class="col-xl-7 col-lg-7 col-md-5">
+<div class="col-xl-7 col-lg-7 col-md-6">
     <div class="descripcion">
         <h2 style="color: #115080;"> {!! $titulo !!} </h2>
         <p class="text-justify " style="font-size:15px;">
