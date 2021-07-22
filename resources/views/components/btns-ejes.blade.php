6f89a6a8dd7d1a86476824daf14fe08e225b416f
JacobLoredo <yeicob_loredo@hotmail.com>
2021-07-22 10:20:41 -0500

    degradado de botones

--- resources/views/components/btns-ejes.blade.php
+++ resources/views/components/btns-ejes.blade.php
@@ -2,19 +2,19 @@
     <div class="row mt-1">
         
         <div class="col-md-3 col-sm-6   mt-1 ">
-            <a class="btn btn-primary  w-100   py-md-2" href="{{route('Gestion')}}" role="button"
+            <a class="btn   w-100   py-md-2" href="{{route('Gestion')}}" role="button"
                 id="btnGestion">GESTIÓN INSTITUCIONAL</a>
         </div>
         <div class="col-md-3 col-sm-6 mt-1">
-            <a class="btn btn-primary  w-100  h-100 py-md-3" href="{{route('Vinculacion')}}" role="button"
+            <a class="btn   w-100  h-100 py-md-3" href="{{route('Vinculacion')}}" role="button"
                 id="btnVinculacion">VINCULACIÓN</a>
         </div>
         <div class="col-md-3 col-sm-6 mt-1 ">
-            <a class="btn btn-primary   w-100 h-100 py-md-3" href="{{route('Comunicacion')}}" role="button"
+            <a class="btn w-100 h-100 py-md-3" href="{{route('Comunicacion')}}" role="button"
                 id="btnComunicacion">COMUNICACIÓN</a>
         </div>
         <div class="col-md-3 col-sm-6 mt-1">
-            <a class="btn btn-primary  w-100 py-md-2" href="{{route('Educacion')}}" role="button"
+            <a class="btn w-100 py-md-2" href="{{route('Educacion')}}" role="button"
                 id="btnEduInv">EDUCACIÓN E INVESTIGACIÓN</a>
         </div>
     </div>
