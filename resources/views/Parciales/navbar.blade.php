00f82f355287818295f07cb1759d6552a96d013b
JacobLoredo <yeicob_loredo@hotmail.com>
2021-08-03 12:32:07 -0500

    cambio de ruta

--- resources/views/Parciales/navbar.blade.php
+++ resources/views/Parciales/navbar.blade.php
@@ -63,7 +63,7 @@
 </nav>
 <nav class="navbar navbar-expand-lg navbar-expand-xl navbar-expand-md d-flex d-lg-flex d-md-flex d-xl-flex">
 
-  <a class="btn btn-success btn-sm MiPortal navbar-brand " href='https://ambiental.uaslp.mx/controlescolar' role="button">
+  <a class="btn btn-success btn-sm MiPortal navbar-brand " href="{{route('login')}}" role="button">
     <img src="{{asset('storage/imagenes/Logos/UBICACION-1.png')}}" alt="" srcset="">
     MI PORTAL
   </a>
@@ -108,7 +108,7 @@
 
 
       <div>
-        <a class="btn btn-primary btn-sm MiPortal" href='https://ambiental.uaslp.mx/controlescolar' role="button">
+        <a class="btn btn-primary btn-sm MiPortal" href='{{route('login')}}' role="button">
           <img src="{{asset('storage/imagenes/Logos/UBICACION-1.png')}}" alt="" srcset="">
           MI PORTAL
         </a>
