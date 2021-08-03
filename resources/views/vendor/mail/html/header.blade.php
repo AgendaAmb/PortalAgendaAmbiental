4c63e8efff474ddbc84949d7c98492441064fa92
JacobLoredo <yeicob_loredo@hotmail.com>
2021-08-03 13:22:06 -0500

    cambios en email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header"style="background-color: #005faf;">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="https://ambiental.uaslp.mx/storage/imagenes/Logos/horizontal%20blanco.png" class="logo" height="100" width="150" alt="Logo agenda/Ualsp">
+<img src="https://ambiental.uaslp.mx/storage/imagenes/Logos/horizontal%20blanco.png" class="logo" height="200" width="250" alt="Logo agenda/Ualsp">
 @if (trim($slot) === 'Laravel')
 @else
 {{ $slot }}
