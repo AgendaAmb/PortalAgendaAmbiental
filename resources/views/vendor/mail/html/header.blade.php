98ecfa92023af8c77c68f626b35a01c9a7254931
JacobLoredo <yeicob_loredo@hotmail.com>
2021-08-03 12:47:03 -0500

    cambio en estulos de correo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,6 @@
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;background-color: red;">
+<td class="header"style="background-color: red;">
+<a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
