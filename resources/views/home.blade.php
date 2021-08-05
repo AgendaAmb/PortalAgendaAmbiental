dfa0d07f29faf106b4fc3783786e5f3bb4a47c56
JacobLoredo <yeicob_loredo@hotmail.com>
2021-08-05 17:42:20 -0500

    cambio de prueba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <!--Camio de poruebas-->
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
