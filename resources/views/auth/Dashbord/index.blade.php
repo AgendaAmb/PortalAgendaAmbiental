6e984d5b1b262e57e82b2dc2b71be059fc682b4b
JacobLoredo <yeicob_loredo@hotmail.com>
2021-08-23 23:00:40 -0500

    cambios para validar

--- resources/views/auth/Dashbord/index.blade.php
+++ resources/views/auth/Dashbord/index.blade.php
@@ -167,7 +167,7 @@
           </button>
         </div>
        
-        <div class="container-fluid bg-white" v-if="TipoUsuario!='externs'?hasModule17Gemas?modalClick!='17Gemas':true:false:false ">
+        <div class="container-fluid bg-white" v-if="TipoUsuario!='externs'?hasModule17Gemas?modalClick=='17Gemas':true:false:false ">
           <div class="row">
             <div class="col-12">
               <img src="{{asset('storage/imagenes/17Gemas/Banner_RegistroCompleto.png')}}" class="img-fluid" alt="">
