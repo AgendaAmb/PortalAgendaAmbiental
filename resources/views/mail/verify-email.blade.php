9e3d843a7839e7496903f1cefc1915a2648448eb
MickeyMenor <miguel.menor09@gmail.com>
2021-08-26 12:09:00 -0500

    Letras grandes en correo de verificacion

--- resources/views/mail/verify-email.blade.php
+++ resources/views/mail/verify-email.blade.php
@@ -7,7 +7,7 @@ Para continuar con el proceso, favor de verificar su correo electrónico en el s
 VERIFICAR EMAIL
 @endcomponent
 
-<br>Agradecemos su registro, y le damos la bienvenida a la comunidad de Agenda Ambiental.<br><br>
+<br>Agradecemos su registro, y le damos la bienvenida a la <h5><strong> comunidad de Agenda Ambiental. </strong></h5><br><br>
 Si tiene algún problema con el botón de acción, <a href="{{ $url }}"> por favor de clic a este enlace </a><br><br><br>
 Saludos cordiales. <br>
 <img src="{{ asset('/storage/imagenes/Logos/rtic.png') }}">
