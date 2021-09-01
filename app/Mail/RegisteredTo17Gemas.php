cfc2182d08414ff47f1a4ff0381f2cab426b3474
MickeyMenor <miguel.menor09@gmail.com>
2021-09-01 12:37:15 -0500

    Correo electronico de registro

---
+++ app/Mail/RegisteredTo17Gemas.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RegisteredTo17Gemas extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(){}
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('rtic.ambiental@uaslp.mx', 'Portal de Agenda Ambiental')
+                    ->subject('Confirmación de registro a 17 Gemas')
+                    ->markdown('mail.modules.registered-to-17-gemas');
+    }
+}
