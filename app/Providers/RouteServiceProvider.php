3fc4af781c408dc76878017ac4d4418c55cf64d7
MickeyMenor <miguel.menor09@gmail.com>
2021-09-01 11:45:58 -0500

    Ruta de MiPortal

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/panel';
+    public const HOME = '/Miportal';
 
     /**
      * Define your route model bindings, pattern filters, etc.
