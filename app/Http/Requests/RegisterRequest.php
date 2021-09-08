d5daa6d89a72bca3669a108e62e63046f660a02a
MickeyMenor <miguel.menor09@gmail.com>
2021-09-08 07:34:42 -0500

    Ajuste a regla del curp en validacion

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -80,7 +80,7 @@ class RegisterRequest extends FormRequest
             'Tel' => [ 'required', 'numeric' ],
             'CURP' => [
                 'nullable',
-                'required_if:Pais,México','size:18',
+                'size:18',
                 'regex:/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/i',
                 'unique:externs,curp',
                 'unique:students,curp',
