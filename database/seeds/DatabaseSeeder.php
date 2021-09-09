40f9f94a7ba1fb4e73f0d1d69620c2d738c3171b
MickeyMenor <miguel.menor09@gmail.com>
2021-09-09 13:26:08 -0500

    Correo masivo enviado; regresando datos en su lugar

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,52 +21,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /*
+
         $this->call([
             ModuleSeeder::class,
             PassportSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
             WorkshopSeeder::class,
-        ]);*/
-
-        $workshop = Workshop::firstWhere('name', 'curso movilidad y urbanismo');
-
-        Student::whereHas('workshops', function($query) use ($workshop){
-            $query->where('id', $workshop->id);
-        })->get()
-        ->each(function($student) use ($workshop){
-            $student->workshops()->updateExistingPivot($workshop->id, [
-                'sent' => true,
-                'sent_at' => Carbon::now()->timezone('America/Mexico_City')
-            ]);
-
-            Mail::to($student)->send(new RegisteredToMMUSConference);
-        });
-
-        Extern::whereHas('workshops', function($query) use ($workshop){
-            $query->where('id', $workshop->id);
-        })->get()
-        ->each(function($extern) use ($workshop){
-            $extern->workshops()->updateExistingPivot($workshop->id, [
-                'sent' => true,
-                'sent_at' => Carbon::now()->timezone('America/Mexico_City')
-            ]);
-
-            Mail::to($extern)->send(new RegisteredToMMUSConference);
-        });
-
-        Worker::whereHas('workshops', function($query) use ($workshop){
-            $query->where('id', $workshop->id);
-        })->orWhereIn('id', [18129, 12457, 7515, 7421, 22524])
-        ->get()
-        ->each(function($worker) use ($workshop){
-            $worker->workshops()->updateExistingPivot($workshop->id, [
-                'sent' => true,
-                'sent_at' => Carbon::now()->timezone('America/Mexico_City')
-            ]);
-
-            Mail::to($worker)->send(new RegisteredToMMUSConference);
-        });
+        ]);
     }
 }
