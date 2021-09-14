948ce84a2c5c53d243e2faea9d22cdd45384aaf8
JacobLoredo <yeicob_loredo@hotmail.com>
2021-09-14 13:45:25 -0500

    cambio en imagenes

--- resources/views/Plantillas/BienvenidaPlantilla.blade.php
+++ resources/views/Plantillas/BienvenidaPlantilla.blade.php
@@ -12,14 +12,15 @@
             <h4 style="color: white;" class="m-0 text-right"> Mi portal</h4>
         </div>
 
-        <div class="col-xl-2  col-md-2  col-4 text-center p-2" style=" border-left: 2px solid white; border-right:2px solid white;">
+        <div class="col-xl-2  col-md-2  col-4 text-center" style=" border-left: 2px solid white; border-right:2px solid white;">
             <a class="text-center" href="{{route('Index')}}">
-                <img src="{{asset('/storage/imagenes/Logos/logoagenda-Bienvenida.png')}}" class="w-75 h-100 ">
+                <img src="{{asset('/storage/imagenes/Logos/logoagenda-Bienvenida.png')}}" class="w-50 h-100" >
             </a>
         </div>
-        <div class="col-xl-2  col-md-2  col-4 my-auto text-center p-2">
-            <a class="" href="#">
-                <img src="{{asset('/storage/imagenes/Logos/LogoUaslp_17Gemas.png')}}" class="w-75 h-75 ">
+        <div class="col-xl-2  col-md-2  col-4 my-auto text-center ">
+          
+            <a class="http://www.uaslp.mx/" target="_blank" href="#">
+                <img src="{{asset('/storage/imagenes/Logos/LogoUaslp_17Gemas.png')}}" class="w-50 h-50 ">
             </a>
         </div>
       
