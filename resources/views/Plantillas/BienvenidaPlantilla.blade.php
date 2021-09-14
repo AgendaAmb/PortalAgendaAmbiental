5721a04443049c39a107f637df227280837c2ce5
JacobLoredo <yeicob_loredo@hotmail.com>
2021-09-14 13:56:49 -0500

    cambio en logos de bienvienida

--- resources/views/Plantillas/BienvenidaPlantilla.blade.php
+++ resources/views/Plantillas/BienvenidaPlantilla.blade.php
@@ -12,15 +12,15 @@
             <h4 style="color: white;" class="m-0 text-right"> Mi portal</h4>
         </div>
 
-        <div class="col-xl-2  col-md-2  col-4 text-center" style=" border-left: 2px solid white; border-right:2px solid white;">
+        <div class="col-xl-1  col-md-2  col-4 text-center" style=" border-left: 2px solid white; border-right:2px solid white;">
             <a class="text-center" href="{{route('Index')}}">
-                <img src="{{asset('/storage/imagenes/Logos/logoagenda-Bienvenida.png')}}" class="w-50 h-100" >
+                <img src="{{asset('/storage/imagenes/Logos/logoagenda-Bienvenida.png')}}" class="w-100 h-100">
             </a>
         </div>
-        <div class="col-xl-2  col-md-2  col-4 my-auto text-center ">
+        <div class="col-xl-1  col-md-2  col-4 my-auto text-center ">
           
             <a class="http://www.uaslp.mx/" target="_blank" href="#">
-                <img src="{{asset('/storage/imagenes/Logos/LogoUaslp_17Gemas.png')}}" class="w-50 h-50 ">
+                <img src="{{asset('/storage/imagenes/Logos/LogoUaslp_17Gemas.png')}}" class="w-100 h-100 ">
             </a>
         </div>
       
