9d91cca02520c2f0c6702faf80b956b767bd11a0
MickeyMenor <miguel.menor09@gmail.com>
2021-09-21 10:09:06 -0500

    APertura solicitada por Marcos

--- resources/views/auth/Dashbord/index.blade.php
+++ resources/views/auth/Dashbord/index.blade.php
@@ -176,14 +176,14 @@
                   Intervenciones y reordenamiento: Cebratón y Proyecto MUS-ZUP
                 </label>
               </div>
-              <!--
+
               <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="Unirodada cicloturística a la Cañada del Lobo"
                   id="Unirodada" v-model="checkedNames">
                 <label class="form-check-label" for="Unirodada">
                   Unirodada cicloturística a la Cañada del Lobo
                 </label>
-              </div>-->
+              </div>
               <br>
             </div>
             <h5 class="modal-title3" id="exampleModalLabel">Datos personales</h5>
