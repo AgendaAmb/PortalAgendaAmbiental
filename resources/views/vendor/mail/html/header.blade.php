f7a69d71791b2abebaf9e2053f207f29826e7aa5
MickeyMenor <miguel.menor09@gmail.com>
2021-09-22 16:57:12 -0500

    Comando solicitado por maru

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -8,7 +8,7 @@ else
 if($header_bottom_color === null) 
     $header_bottom_color = 'background-color: #012878;';
 else 
-    $header_bottom_color = 'display:block; background-color: '.$header_bottom_color.'; width: 100%; height: 20px;';
+    $header_bottom_color = 'display:block; background-color: '.$header_bottom_color.'; width: 100%; height: 20px; margin-top: -3px';
 
 @endphp
 
@@ -21,6 +21,6 @@ else
         </a>
     </td>
 </tr>
-<tr style="padding-top: -3px;">
-    <td style="{{ $header_bottom_color }}"></td>
+<tr style="{{ $header_bottom_color }}">
+    <td></td>
 </tr>
