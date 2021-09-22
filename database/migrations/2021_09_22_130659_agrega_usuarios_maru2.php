31df7c4f728310c0c9927a343b168cdb30ea4c05
MickeyMenor <miguel.menor09@gmail.com>
2021-09-22 13:11:42 -0500

    Cambios de maru

---
+++ database/migrations/2021_09_22_130659_agrega_usuarios_maru2.php
@@ -0,0 +1,52 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AgregaUsuariosMaru2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('database:backup');
+
+        $idWorkshop = DB::table('user_workshop')
+        ->insertGetId([
+            'user_id' => 51,
+            'user_type' => Extern::class,
+            'workshop_id' => 6,
+            'paid' => true,
+            'paid_at' => Carbon::now(),
+        ]);
+
+        DB::table('unirodada_users')
+        ->insert([
+            'user_workshop_id' => $idWorkshop,
+            'emergency_contact' => 'LUIS ENRIQUE MEJIA ESTRADA',
+            'emergency_contact_phone' => '4441868243',
+            'group' => 'ninguno',
+            'health_condition' => 'CondicionBuena',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'lunch' => 0,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
