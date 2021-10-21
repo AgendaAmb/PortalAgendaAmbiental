ba8704e5972e42b0267552501a5c1fbb26bc42ff
MickeyMenor <miguel.menor09@gmail.com>
2021-10-21 14:23:35 -0500

    Datos de facturacion

---
+++ database/migrations/2021_10_21_140313_create_invoice_data_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_data', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->string('user_type');
+            $table->string('rfc')->nullable();
+            $table->string('email')->nullable();
+            $table->string('name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->primary(['user_id','user_type']);
+            $table->foreign(['user_id', 'user_type'])
+                ->references(['id', 'type'])
+                ->on('users')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_data');
+    }
+}
