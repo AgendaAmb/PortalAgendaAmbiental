930fb86bbebe71c2d298e550b6a1795dd7023254
JacobLoredo <yeicob_loredo@hotmail.com>
2021-11-11 12:41:29 -0600

    migracion

---
+++ database/migrations/2021_11_11_171805_create_user_taller_huerto.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Auth\User;
+use App\Models\Workshop;
+class CreateUserTallerHuerto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $workshop_model = Workshop::firstWhere('name', 'Agricultura urbana ¿Qué? ¿Cuándo? ¿Cómo? ¿Por qué?(27 Noviembre)');
+
+        $user=User::find(25137);
+        $user->assignWorkshop($workshop_model->id);
+
+        
+        $user2=User::find(278452);
+        $user2->assignWorkshop($workshop_model->id);
+     
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_taller_huerto');
+    }
+}
