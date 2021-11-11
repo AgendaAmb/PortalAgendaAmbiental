346c7cde7b4172d150e45b3ae919aae28e29edeb
JacobLoredo <yeicob_loredo@hotmail.com>
2021-11-11 12:15:54 -0600

    cambio de redaccion

--- resources/views/mail/workshops/send-receipt.blade.php
+++ resources/views/mail/workshops/send-receipt.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message', [ 'header_color' => $header_color, 'font_color' => $footer_color])
 # Estimado(a) usuario(a):<br><br>
 
-Gracias por preinscribirse a la  Unirodada cicloturística a la Cañada del Lobo. <br><br>
+Gracias por preinscribirse a la Taller: Agricultura urbana ¿Qué? ¿Cuándo? ¿Cómo? ¿Por qué? . <br><br>
 
 Anexo le estamos enviando su ficha de pago y le recordamos que tiene cuatro días
-hábiles para realizar su pago y subir su comprobante en la sección de AVISOS en Mi Portal. <br><br>
+hábiles para realizar su pago y enviar su comprabante <br><br>
 
 NOTA: Queda oficialmente inscrito (a) una vez hecho el pago. <br><br>
 
