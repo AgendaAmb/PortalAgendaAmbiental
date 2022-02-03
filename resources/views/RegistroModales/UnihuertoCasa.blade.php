f7449a846368cd82a6f849ccf945d3beb94ebe5f
piktamine <49212598+piktamine@users.noreply.github.com>
2022-02-03 14:15:22 -0600

    unitrueque registroo

--- resources/views/RegistroModales/UnihuertoCasa.blade.php
+++ resources/views/RegistroModales/UnihuertoCasa.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header bg-primary" id="modalGemas">
-          <h5 class="modal-title mx-auto text-white">UNIHUERTO EN CASA FEBRERO - MAYO 2020</h5>
+          <h5 class="modal-title mx-auto text-white">UNIHUERTO EN CASA FEBRERO - MAYO 2022</h5>
 
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
