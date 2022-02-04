9c8c9034768090931304a97f8315f687032de158
AgendaAmb <91550278+AgendaAmb@users.noreply.github.com>
2022-02-04 09:47:48 -0600

    Plantilla correo

--- resources/views/mail/workshops/send-receipt.blade.php
+++ resources/views/mail/workshops/send-receipt.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message', [ 'header_color' => $header_color, 'font_color' => $footer_color])
 # Estimado(a) usuario(a):<br><br>
 
-Gracias por preinscribirse a la Taller: Agricultura urbana ¿Qué? ¿Cuándo? ¿Cómo? ¿Por qué? . <br><br>
+Gracias por preinscribirse al Curso-Taller: Unihuerto en casa. <br><br>
 
 Anexo le estamos enviando su ficha de pago y le recordamos que tiene cuatro días
 hábiles para realizar su pago y enviar su comprabante <br><br>
@@ -11,13 +11,13 @@ NOTA: Queda oficialmente inscrito (a) una vez hecho el pago. <br><br>
 Estamos a su disposición para cualquier duda o comentario, <br><br>
 
 IBP Laura Daniela Hernández Rodríguez <br>
-UNIBICI
+Programa Uihuerto
 
 @isset($with_image)
 @if ($with_image !== false)
-<img src="{{ asset('/storage/imagenes/Logos/rtic.png') }}">
+<img src="{{ asset('/storage/imagenes/Logos/SGA.png') }}">
 @endif
 @else
-<img src="{{ asset('/storage/imagenes/Logos/rtic.png') }}">
+<img src="{{ asset('/storage/imagenes/Logos/SGA.png') }}">
 @endisset
 @endcomponent
