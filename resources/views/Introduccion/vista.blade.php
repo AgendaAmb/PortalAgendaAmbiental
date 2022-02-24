bcc723cd6c680ada5dd987e5cc11d0713f127d9a
AgendaAmb <91550278+AgendaAmb@users.noreply.github.com>
2022-02-24 13:13:09 -0600

    Index Del Huerto a la Mesa

--- resources/views/Introduccion/vista.blade.php
+++ resources/views/Introduccion/vista.blade.php
@@ -64,6 +64,35 @@
           <div class="d-none d-lg-block d-md-block">
             <div class="slide-box">
              
+              <a href={{route('Gestion',['nombreModal'=> 'modalTallerHuertoalaMes'])}}>
+                <img src="{{asset('/storage/imagenes/Unihuerto/B_HuertoMesa1.png')}}" class="imgCaoursel "
+                  alt="First slide">
+              </a>
+              <a href={{route('Gestion',['nombreModal'=> 'modalTallerHuertoalaMes'])}}>
+                <img src="{{asset('/storage/imagenes/Unihuerto/B_HuertoMesa2.png')}}" class="imgCaoursel "
+                  alt="First slide">
+              </a>
+            </div>
+          </div>
+
+          <div class="d-none d-sm-block d-md-none">
+            <div class="slide-box">
+              
+              <a href={{route('Gestion',['nombreModal'=> 'modalTallerHuertoalaMes'])}}>
+                <img src="{{ asset('/storage/imagenes/Unihuerto/B_HuertoMesa1.png')}}" class="imgCaoursel "
+                  alt="First slide">
+              </a>
+              <a href={{route('Gestion',['nombreModal'=> 'modalTallerHuertoalaMes'])}}>
+                <img src="{{asset('/storage/imagenes/Unihuerto/B_HuertoMesa2.png')}}" class="imgCaoursel "
+                  alt="First slide">
+              </a>
+            </div>
+          </div>
+
+        <div class="carousel-item">
+          <div class="d-none d-lg-block d-md-block">
+            <div class="slide-box">
+             
               <a href={{route('Gestion',['nombreModal'=> 'pdf'])}}>
                 <img src="{{asset('/storage/imagenes/PUR/Banner1.png')}}" class="imgCaoursel "
                   alt="First slide">
@@ -87,7 +116,7 @@
                   alt="First slide">
               </a>
             </div>
-          </div>
+          </div>          
 
         </div>
         <div class="carousel-item ">
---
+++ resources/views/mail/workshops/send-receipt-huertomesa.blade.php
@@ -0,0 +1,23 @@
+@component('mail::message', [ 'header_color' => $header_color, 'font_color' => $footer_color])
+Estimado(a) usuario(a):<br><br>
+
+Gracias por preinscribirse al Mini-Taller: Del huerto a la mesa. <br><br>
+
+Anexo le estamos enviando su ficha de pago y le recordamos que tiene cuatro días
+hábiles para realizar su pago y enviar su comprobante. <br><br>
+
+NOTA: Queda oficialmente inscrito (a) una vez hecho el pago. <br><br>
+
+Estamos a su disposición para cualquier duda o comentario, <br><br>
+
+IBP Laura Daniela Hernández Rodríguez <br>
+Programa Uihuerto
+
+@isset($with_image)
+@if ($with_image !== false)
+<img src="{{ asset('/storage/imagenes/Logos/SGA.png') }}">
+@endif
+@else
+<img src="{{ asset('/storage/imagenes/Logos/SGA.png') }}">
+@endisset
+@endcomponent
