b80b7410491c208f6bad597ca3024085b492c11e
Oscarucho2 <oskarmartinez15@gmail.com>
2023-10-02 18:43:59 -0600

    cambiando redaccion a correo general

--- resources/views/mail/workshops/registered-workshops.blade.php
+++ resources/views/mail/workshops/registered-workshops.blade.php
@@ -13,6 +13,6 @@ Muchas gracias por registrarse a las siguientes actividades :<br><br>
     @endif
 
 
-Agradecemos su preregistro. Se le enviará un correo con las indicaciones para la actividad registrada. <br><br><br>
+Agradecemos su registro.<br><br><br>
 Saludos cordiales. <br>
 @endcomponent
